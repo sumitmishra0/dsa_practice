be6bba93c7f82c41cc4338b4ad384cf904e10347
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-05-05 05:13:49 +0530

    nth element in a fibonacci series

---
+++ nth_element_in_fibonacci.cpp
@@ -0,0 +1,25 @@
+// find nth element in a fibonacci series 
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+    int arr[1000]; // We avoid to take like this arr[n] because it may failed in some test cases so for example we have taken arr[1000]
+    
+    // first = 0;
+    // second = 1;
+    
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(int i = 2; i <= n-1; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    
+    cout << arr[n-1] << " ";
+    
+}
