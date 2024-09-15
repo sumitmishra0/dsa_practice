2d5c684188c4737e9fe12341fe721a8a1b756232
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-09-15 09:09:04 +0530

    Create pass_by_reference.cpp

---
+++ pass_by_reference.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int &a,int &b){
+	a++;
+	return a+b;
+}
+int main() {
+	int x = 9, y = 18;
+	cout << func(x,y) << endl;
+	cout << "x : " << x << endl;  // value of x will be update to 10 because we have passed a reference of x in func
+	return 0;
+}
