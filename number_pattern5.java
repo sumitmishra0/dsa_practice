5f93b4eb4ca856e92f1b925a6a816fd96aeb6255
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-09-15 09:24:48 +0530

    Create number_pattern5.java

---
+++ number_pattern5.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+        // Note: Don't change class name
+        // your code goes here
+
+			// 12345
+			// 2345
+			// 345
+			// 45
+			// 5
+			
+		int n = 5;
+
+		for(int i = 1; i <= n; i++){
+			// System.out.println(i);
+			for(int j = i; j <= n; j++){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+    }
+}
