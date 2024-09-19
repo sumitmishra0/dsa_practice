23fca7c00541be978f896e3aa683bf31d04dee2c
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-09-19 07:37:32 +0530

    Create basics_of_array.cpp

---
+++ basics_of_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int marks[5];
+
+	for(int i = 0; i < 5; i++){
+		cin>>marks[i];
+	}
+	cout << marks[4]<< endl;
+	cout << 4[marks] << endl;
+	cout << marks << endl;
+	cout << *marks << endl;
+	cout << *(marks+1) << endl;
+
+
+	return 0;
+}
