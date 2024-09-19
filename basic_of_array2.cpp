96fd1cd3a1f1392aa6b13111ed03725f57226c90
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-09-19 07:38:00 +0530

    Create basic_of_array2.cpp

---
+++ basic_of_array2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	float float_ar[10];
+	cout << float_ar << endl;
+	cout << (float_ar + 1) << endl;
+	cout << sizeof(float) << endl;
+    cout << endl;
+
+	cout <<"double " << endl;
+    double double_ar[10];
+	cout << double_ar << endl;
+	cout << (double_ar + 1) << endl;
+	cout << sizeof(double);
+	return 0;
+}
